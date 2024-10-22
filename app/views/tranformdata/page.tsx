542aab3ce4f7347fd7ce2742eeb7c8aa7dd37dfc
NatawatDev <natawat.codealong@gmail.com>
2024-10-22 17:32:45 +0700

    add tranformdata

--- app/views/tranformdata/page.tsx
+++ app/views/tranformdata/page.tsx
@@ -2,30 +2,77 @@
 
 import React, { useState, useEffect } from 'react'
 
-const tranformdata = async() => {
+const tranformdata = () => {
   const [userData, setUserData] = useState([])
 
-  const response = await fetch('https://dummyjson.com/users')
-  let result = response.json
-  // console.log(result);
-  // const fetchUserData = async () => {
-  //   try {
-  //     const response = await fetch('https://dummyjson.com/users')
-  //     setUserData(response.json())
-  //   } catch (error) {
-  //     console.log(error);
-  //   }    
-  // }
+  const fetchUserData = async () => {
+    try {
+      const response = await fetch('https://dummyjson.com/users')
+      const result = await response.json()
+      tranformData(result)
+    } catch (error) {
+      console.log(error);
+    }    
+  }
 
-  // useEffect(() => {
-  //   console.log(userData);
-  // }, [])
+  const tranformData = (responseValue) => {
+    const users = responseValue.users
+    const result = {}
+    const arrOfAge = []
+    users.forEach((user) => {
+      const { company, address, age, hair, firstName, lastName, gender } = user
+      const department = company.department
+      const potalCode = address.postalCode
+      const hairColor = hair.color
+      const fullName = `${firstName}${lastName}`
+      arrOfAge.push(age)
+      const minAge = Math.min(...arrOfAge)
+      const maxAge = Math.max(...arrOfAge)
+      if (!result[department]) {
+        result[department] = {
+          male: 0,
+          female: 0,
+          ageRange: "",
+          hair: {
+            Black: 0,
+            Blond: 0,
+            Chestnut: 0,
+            Brown: 0,
+            Gray: 0,
+            Red: 0,
+            White: 0,
+            Green: 0,
+            Purple: 0
+          },
+          addressUser: {},
+        }        
+      }
+      result[department][gender]++;
+      result[department].hair[hairColor]++;    
+      result[department].addressUser[fullName] = potalCode;
+      result[department].ageRange = minAge+'-'+maxAge      
+                  
+    })
+    
+    setUserData((prev) => [...prev, result])
+
+    console.log(userData);
+    
+  }
+
+  useEffect(() => {
+    fetchUserData()
+  }, [])
 
 
   return (
     <div>
+      <h1>User Data</h1>
+      {/* <div>
+        {userData}
+      </div> */}
     </div>
-  )
+  );
 }
 
 export default tranformdata
